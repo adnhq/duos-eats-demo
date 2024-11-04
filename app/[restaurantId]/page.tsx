721667b21729ae316ff4c9b96e86d5cbd7759827
adnhq <adh.xeph@gmail.com>
2024-11-05 00:38:16 +0600

    escusi

--- app/[restaurantId]/page.tsx
+++ app/[restaurantId]/page.tsx
@@ -1,13 +1,15 @@
 import RestaurantMenu from "@/components/RestaurantMenu";
 import { getRestaurant } from "@/lib/actions";
-import React from "react";
 
-type Params = {
-  restaurantId: string;
-};
+interface PageProps {
+  params: {
+    restaurantId: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+}
 
-export default async function Page({ params }: { params: Params }) {
-  const { restaurantId } = params; 
+export default async function Page({ params }: PageProps) {
+  const { restaurantId } = params;
   const restaurant = await getRestaurant(restaurantId);
 
   if (restaurant.length === 0) return <h1>No Restaurant found</h1>;

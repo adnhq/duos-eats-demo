12c21222e27152f42a7fd865a661b1e1daab5735
Neloy Saha <neloy.saha456@gmail.com>
2024-11-05 01:22:00 +0600

    build

--- app/[restaurantId]/page.tsx
+++ app/[restaurantId]/page.tsx
@@ -1,18 +1,16 @@
 import RestaurantMenu from "@/components/RestaurantMenu";
 import { getRestaurant } from "@/lib/actions";
+import React from "react";
 
-// Correct way to type params in a Next.js page component
-type Props = {
-  params: {
-    restaurantId: string;
-  };
-};
-
-export default async function Page({ params }: Props) {
-  const { restaurantId } = params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ restaurantId: string }>;
+}) {
+  const { restaurantId } = await params;
   const restaurant = await getRestaurant(restaurantId);
 
-  if (!restaurant.length) return <h1>No Restaurant found</h1>;
+  if (restaurant.length === 0) return <h1>No Restaurant found</h1>;
 
   return <RestaurantMenu restaurantData={restaurant[0]} />;
 }

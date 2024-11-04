cf05fea1f6387af1e594bafcff14b9c3de1395e9
Neloy Saha <neloy.saha456@gmail.com>
2024-11-05 00:46:26 +0600

    b1

--- app/[restaurantId]/page.tsx
+++ app/[restaurantId]/page.tsx
@@ -2,15 +2,17 @@ import RestaurantMenu from "@/components/RestaurantMenu";
 import { getRestaurant } from "@/lib/actions";
 import React from "react";
 
-type Params = {
-  restaurantId: string;
+type Props = {
+  params: {
+    restaurantId: string;
+  };
 };
 
-export default async function Page({ params }: { params: Params }) {
-  const { restaurantId } = params; 
+export default async function Page({ params }: Props) {
+  const { restaurantId } = params;
   const restaurant = await getRestaurant(restaurantId);
 
-  if (restaurant.length === 0) return <h1>No Restaurant found</h1>;
+  if (!restaurant.length) return <h1>No Restaurant found</h1>;
 
   return <RestaurantMenu restaurantData={restaurant[0]} />;
 }

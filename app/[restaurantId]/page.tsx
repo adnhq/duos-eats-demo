82c1f95e6eadad1b14ed5e7871f17f7bff86d4dd
adnhq <adh.xeph@gmail.com>
2024-11-05 00:40:38 +0600

    biswas

--- app/[restaurantId]/page.tsx
+++ app/[restaurantId]/page.tsx
@@ -1,14 +1,11 @@
 import RestaurantMenu from "@/components/RestaurantMenu";
 import { getRestaurant } from "@/lib/actions";
 
-interface PageProps {
-  params: {
-    restaurantId: string;
-  };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export default async function Page({ params }: PageProps) {
+export default async function Page({
+  params,
+}: {
+  params: { restaurantId: string }
+}) {
   const { restaurantId } = params;
   const restaurant = await getRestaurant(restaurantId);
 
